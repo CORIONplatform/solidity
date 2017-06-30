2fefad1b74b1372f951f6385d26fd194d4e276db
iFA <ifa@ethereumlottery.net>
2017-06-30 21:46:58 +0200

    Update owned.sol
    `owned` was useless.

--- owned.sol
+++ owned.sol
@@ -1,30 +1,5 @@
 pragma solidity ^0.4.11;
 
-contract owned {
-    address private owner = msg.sender;
-    
-    function replaceOwner(address newOwner) external returns(bool) {
-        /*
-            Owner replace.
-            
-            @newOwner   Address of new owner.
-        */
-        require( isOwner() );
-        owner = newOwner;
-        return true;
-    }
-    
-    function isOwner() internal returns(bool) {
-        /*
-            Check of owner address.
-            
-            @bool   Owner has called the contract or not 
-        */
-        return owner == msg.sender;
-    }
-}
-
-
 contract ownedDB {
     address private owner;
     
