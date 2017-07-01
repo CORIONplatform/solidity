adcb0b831e0fefc14afe27cd4c81ee78889aea3e
iFA <ifa@ethereumlottery.net>
2017-07-01 23:30:03 +0200

    Update ico.sol

--- ico.sol
+++ ico.sol
@@ -25,7 +25,7 @@ contract ico is safeMath {
         uint256 corp;
     }
     
-    uint256 constant oneSegment = 40320;
+    uint256 constant oneSegment = 40320;https://github.com/CORIONplatform/solidity
     
     address public owner;
     address public tokenAddr;
@@ -364,7 +364,7 @@ contract ico is safeMath {
                 x = (value * 1e6 * USD_ETC_exchange rate / 1e4 / 1e18) * bonus percentage
                 2.700000 token = (1e18 * 1e6 * 22500 / 1e4 / 1e18) * 1.20
         */
-        reward = (value * 1e6 * icoExchangeRate / icoExchangeRateM / 1 ether) * (ICObonus + 100) / 100;
+        reward = (value * 1e6 * icoExchangeRate / icoExchangeRateM / 1 ether) * (ICObonus() + 100) / 100;
         if ( reward < 5e6) { return 0; }
     }
     
