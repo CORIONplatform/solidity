9d5b10b75e88587654e6000e175caedfbe500e07
iFA <ifa@ethereumlottery.net>
2017-07-05 21:53:04 +0200

    Update token.sol

--- token.sol
+++ token.sol
@@ -16,6 +16,7 @@ contract token is safeMath, module, announcementTypes {
         module callbacks
     */
     function connectModule() external returns (bool success) {
+        require( super.isModuleHandler(msg.sender) );
         super._connectModule();
         isICO = db.isICO();
         return true;
