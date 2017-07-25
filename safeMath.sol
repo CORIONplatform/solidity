ff600c696224951e09f34a08011b9dbf78d885cf
iFA <ifa@ethereumlottery.net>
2017-07-25 12:01:26 +0200

    Update safeMath.sol

--- safeMath.sol
+++ safeMath.sol
@@ -1,15 +1,10 @@
+/*
+    safeMath.sol
+*/
 pragma solidity ^0.4.11;
 
 contract safeMath {
     function safeAdd(uint256 a, uint256 b) internal returns(uint256) {
-        /*
-            Biztonsagos hozzadas. Tulcsordulas elleni vedelem.
-            A vegeredmeny nem lehet kevesebb mint az @a, ha igen megall a kod.
-            
-            @a          Amihez hozzaadni kell
-            @b          Amennyit hozzaadni kell.
-            @uint256    Vegeredmeny.
-        */
         if ( b > 0 ) {
             assert( a + b > a );
         }
@@ -17,14 +12,6 @@ contract safeMath {
     }
         
     function safeSub(uint256 a, uint256 b) internal returns(uint256) {
-        /*
-            Biztonsagos kivonas. Tulcsordulas elleni vedelem.
-            A vegeredmeny nem lehet tobb mint az @a, ha igen megall a kod.
-            
-            @a          Amibol kivonni kell.
-            @b          Amennyit kivonni kell.
-            @uint256    Vegeredmeny.
-        */
         if ( b > 0 ) {
             assert( a - b < a );
         }
