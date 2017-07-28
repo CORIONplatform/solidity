a823ffafa3506d75669f485da924ad65393bf1e1
iFA <ifa@ethereumlottery.net>
2017-07-28 19:57:15 +0200

    Update publisher.sol

--- publisher.sol
+++ publisher.sol
@@ -58,11 +58,11 @@ contract publisher is announcementTypes, module, safeMath, moduleMultiOwner {
     mapping(uint256 => announcements_s) public announcements;
     mapping (address => uint256[]) public opponents;
     /* Constructor */
-    function publisher(address moduleHandler) moduleMultiOwner(moduleHandler) module(moduleHandlerAddress) {
+    function publisher(address moduleHandlerAddress) moduleMultiOwner(moduleHandlerAddress) module(moduleHandlerAddress) {
         /*
             Installation function. The installer will be registered in the admin list automatically.
             
-            @moduleHandler      Address of moduleHandler
+            @moduleHandlerAddress   Address of moduleHandler
         */
     }
     /* Externals */
