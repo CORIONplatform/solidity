12ee5330d91e69432f0c83f2130109e8ac5625f5
iFA <ifa@ethereumlottery.net>
2017-07-29 12:29:32 +0200

    Update thirdPartyContract.sol

--- thirdPartyContract.sol
+++ thirdPartyContract.sol
@@ -3,14 +3,9 @@
 */
 pragma solidity ^0.4.11;
 
-contract TPCCOR {
+contract thirdPartyContract {
     function CORAddress() constant public returns (address) {}
-    function receiveCOR(address, uint256, bytes) external returns (bool, uint256) {}
-    function approvedCOR(address, uint256, bytes) external returns (bool) {}
-}
-
-contract TPCCORP {
     function CORPAddress() constant public returns (address) {}
-    function receiveCORP(address, uint256, bytes) external returns (bool, uint256) {}
-    function approvedCORP(address, uint256, bytes) external returns (bool) {}
+    function receiveToken(address, uint256, bytes) external returns (bool, uint256) {}
+    function approvedToken(address, uint256, bytes) external returns (bool) {}
 }
