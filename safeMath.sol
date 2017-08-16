0d70c8e7a698019b9a4912efc893e29acc22738f
iFA <ifa@ethereumlottery.net>
2017-08-16 17:25:29 +0200

    safeMul set to internal

--- safeMath.sol
+++ safeMath.sol
@@ -16,7 +16,7 @@ contract safeMath {
         }
         return a - b;
     }
-    function safeMul(uint256 a, uint256 b) returns (uint256) {
+    function safeMul(uint256 a, uint256 b) internal returns (uint256) {
         uint256 c = a * b;
         assert(a == 0 || c / a == b);
         return c;
